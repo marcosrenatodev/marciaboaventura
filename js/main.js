fe2f67f5b0363557a6978b741eb91359f4683faa
Mr-RenatoBlue <renatomedeirosrocha@gmail.com>
2025-07-25 22:12:53 -0300

    feat:blog

--- js/main.js
+++ js/main.js
@@ -72,23 +72,3 @@ window.ontouchmove = function () {
   navbarMenu.classList.remove("show");
 };
 
-function configurarCartaoExpandido(cardId) {
-  const card = document.getElementById(cardId);
-  const closeButton = card.querySelector(".close-card");
-
-  card.onclick = function (event) {
-    if (!card.classList.contains("expanded")) {
-      card.classList.add("expanded");
-    }
-  };
-
-  closeButton.onclick = function (event) {
-    event.stopPropagation();
-    card.classList.remove("expanded");
-  };
-}
-
-configurarCartaoExpandido("card-direito-civil");
-configurarCartaoExpandido("card-direito-consumidor");
-configurarCartaoExpandido("card-direito-penal");
-configurarCartaoExpandido("card-direito-familia");
